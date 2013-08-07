7a5e6ed3a11b930737081984fa5dae4e89ae3a6d
Jack Viers <jackcviers@gmail.com>
2013-08-07 18:49:41 -0500

    Refactors member names to be clearer

--- src/main/scala/zzz/akka/avionics/Altimeter.scala
+++ src/main/scala/zzz/akka/avionics/Altimeter.scala
@@ -12,24 +12,24 @@ class Altimeter extends Actor with ActorLogging {
 
   implicit val ec = context.dispatcher
 
-  val ceiling = 43000
-  val maxRateOfClimb = 5000
+  val ceilingInFeet = 43000 // in feet
+  val maxRateOfClimbInFeetPerMinute = 5000 // in feet/min
   val ticker = context.system.scheduler.schedule(100.millis, 100.millis, self, Tick)
 
-  var rateOfClimb = 0f
-  var altitude = 0d
-  var lastTick = System.currentTimeMillis
+  var currentRateOfClimbInFeetPerMinute = 0f
+  var currentAltitudeInFeet = 0d
+  var timeSinceLastTick = System.currentTimeMillis
 
   case object Tick
 
   def receive = {
     case RateChange(amount) ⇒
-      rateOfClimb = amount.min(1.0f).max(-1.0f) * maxRateOfClimb
-      log info (s"Altimeter changed rate of climb to $rateOfClimb.")
+      currentRateOfClimbInFeetPerMinute = amount.min(1.0f).max(-1.0f) * maxRateOfClimbInFeetPerMinute
+      log info (s"Altimeter changed rate of climb to $currentRateOfClimbInFeetPerMinute.")
     case Tick ⇒
       val tick = System.currentTimeMillis
-      altitude = altitude + ((tick - lastTick) / 60000.0) * rateOfClimb
-      lastTick = tick
+      currentAltitudeInFeet = currentAltitudeInFeet + ((tick - timeSinceLastTick) / 60000.0) * currentRateOfClimbInFeetPerMinute
+      timeSinceLastTick = tick
   }
 
   override def postStop() = ticker.cancel
