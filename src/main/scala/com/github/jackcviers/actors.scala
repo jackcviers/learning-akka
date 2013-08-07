b491361137ae9512a9f2c0fc5567cd74184d649d
Jack Viers <jackcviers@gmail.com>
2013-08-07 18:14:29 -0500

    Adds Mine actor to respond to Alpha messages.

--- src/main/scala/com/github/jackcviers/actors.scala
+++ src/main/scala/com/github/jackcviers/actors.scala
@@ -23,11 +23,22 @@ class Printing extends Actor {
   }
 }
 
+class Mine extends Actor {
+  def receive = {
+    case "Hello" ⇒ println("Him: Hi")
+    case 42 ⇒ println("Him: I don't know the question. Go ask the Earth Mark II.")
+    case s: String ⇒ println(s"Him: You sent me a string: $s.")
+    case Alpha(Beta(b1, Gamma(g1)), Beta(b2, Gamma(g2))) ⇒ println(s"Him: beta1: $b1, beta2: $b2, gamma1: $g1, gamma2: $g2")
+    case _ ⇒ println("Him: Huh?")
+  }
+}
+
 object BadShakespeareanMain {
   val system = ActorSystem("BadShakespearean")
   val actor = system.actorOf(Props[BadShakespearean], "Shake")
   val wood = system.actorOf(Props[Wood], "Wood")
   val printing = system.actorOf(Props[Printing], "Printing")
+  val mine = system.actorOf(Props[Mine], "Mine")
   def send(msg: String) {
     println(s"Me: $msg")
     actor ! msg
@@ -42,11 +53,25 @@ object BadShakespeareanMain {
       printing ! a
     }
   }
+  def sendMine() {
+    println("Me: Hello")
+    mine ! "Hello"
+    Thread.sleep(100)
+    println("Me: 42")
+    mine ! 42
+    Thread.sleep(100)
+    println("Me: Alpha!")
+    mine ! Alpha(b1 = Beta(b = "A", g = Gamma(g = "Z")), b2 = Beta(b = "B", g = Gamma(g = "Y")))
+    Thread.sleep(100)
+    println("Me: Gamma(How much wood could a woodchuck chuck if a woodchuck could chuck wood.)")
+    mine ! Gamma(g = "How much wood could a woodchuck chuck if a woodchuck could chuck wood.")
+  }
   def main(args: Array[String]) {
     send("Good Morning")
     sendPrint()
     send("You're terrible")
     sendWood("If a tree falls in a forest, does it make a sound?")
+    sendMine()
     system.shutdown()
   }
 }
