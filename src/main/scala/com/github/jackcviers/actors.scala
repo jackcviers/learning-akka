eb13b43af6a53a6e95cebd0709d3fe02921188d4
Jack Viers <jackcviers@gmail.com>
2013-08-07 17:47:36 -0500

    Adds Wood actor.

--- src/main/scala/com/github/jackcviers/actors.scala
+++ src/main/scala/com/github/jackcviers/actors.scala
@@ -11,17 +11,29 @@ class BadShakespearean extends Actor {
   }
 }
 
+class Wood extends Actor {
+  def receive = {
+    case _ ⇒ throw new Exception("Wood cannot hear you.")
+  }
+}
+
 object BadShakespeareanMain {
   val system = ActorSystem("BadShakespearean")
   val actor = system.actorOf(Props[BadShakespearean], "Shake")
+  val wood = system.actorOf(Props[Wood], "Wood")
   def send(msg: String) {
     println(s"Me: $msg")
     actor ! msg
     Thread.sleep(100)
   }
+  def sendWood(msg: String) {
+    println(s"Me: $msg")
+    wood ! msg
+  }
   def main(args: Array[String]) {
     send("Good Morning")
     send("You're terrible")
+    sendWood("If a tree falls in a forest, does it make a sound?")
     system.shutdown()
   }
 }
