5a958cfceb99ebd551271c9162bb07099d031c26
Jack Viers <jackcviers@gmail.com>
2013-08-08 20:57:06 -0500

    Implements tests for the Altimeter actor.

--- src/test/scala/zzz/akka/avionics/AltimeterSpec.scala
+++ src/test/scala/zzz/akka/avionics/AltimeterSpec.scala
@@ -1,16 +1,51 @@
 package zzz.akka.avionics
 
-import akka.actor.Actor
-import akka.actor.ActorSystem
-import akka.actor.Props
-import akka.testkit.TestActorRef
-import akka.testkit.TestLatch
-import akka.testkit.{ ScopedTestKit, TestKitBase }
+import akka.actor.{ Actor, ActorSystem, Props }
+import akka.testkit.{ ScopedTestKit, TestActorRef, TestKitBase, TestLatch }
 import org.specs2.mutable.Specification
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class AltimeterSpec extends Specification {
   isolated
+  import Altimeter._
+  override def is = {
+    s2"""
+  Altimeter should
+    record rate of climb changes $recordRateOfClimb
+    keep rate of climb changes within bounds $keepRateOfClimbChangesWithinBounds
+    calculateAltitudeChanges $calculateAltitudeChanges
+    and sendEvents $sendEvents
+"""
+  }
 
+  def recordRateOfClimb = new altimeterContext {
+    val (_, real) = actor()
+    real receive RateChange(1f)
+    real.currentRateOfClimbInFeetPerMinute must_== real.maxRateOfClimbInFeetPerMinute
+  }
+
+  def keepRateOfClimbChangesWithinBounds = new altimeterContext {
+    val (_, real) = actor()
+    real receive RateChange(2f)
+    real.currentRateOfClimbInFeetPerMinute must_== real.maxRateOfClimbInFeetPerMinute
+  }
+
+  def calculateAltitudeChanges = new altimeterContext {
+    val ref = system.actorOf(Props(Altimeter()))
+    ref ! EventSource.RegisterListener(testActor)
+    ref ! RateChange(1f)
+    fishForMessage() {
+      case AltitudeUpdate(altitude) if (altitude) == 0f ⇒ false
+      case AltitudeUpdate(altitude) ⇒ true
+    }
+  }
+
+  def sendEvents = new altimeterContext {
+    val (ref, _) = actor()
+    Await ready (EventSourceSpy.latch, "1" seconds)
+    EventSourceSpy.latch.isOpen must_== true
+  }
 }
 
 trait altimeterContext extends ScopedTestKit with TestKitBase { scope ⇒
