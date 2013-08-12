57914a219a3bd89da632e86f4048fb6cf8dbf76a
Jack Viers <jackcviers@gmail.com>
2013-08-11 21:44:34 -0500

    Populates plane with flightcrew.

--- src/main/scala/zzz/akka/avionics/Plane.scala
+++ src/main/scala/zzz/akka/avionics/Plane.scala
@@ -2,6 +2,7 @@ package zzz.akka.avionics
 
 import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
 
+
 object Plane {
   case object GiveMeControl
   case class Controls(controls: ActorRef)
@@ -12,20 +13,28 @@ class Plane extends Actor with ActorLogging {
   import Altimeter._
   import ControlSurfaces._
   import Plane._
+  import Pilot._
 
+  val configKey = "zzz.akka.avionics.flightcrew"
   val altimeter = context.actorOf(Props(Altimeter()), "Altimeter")
   val controls = context.actorOf(Props(ControlSurfaces(altimeter)), "ControlSurfaces")
+  val config = context.system.settings.config
+  val pilot = context.actorOf(Props(Pilot()), config.getString(s"$configKey.pilotName"))
+  val copilot = context.actorOf(Props(Copilot()), config.getString(s"$configKey.copilotName"))
+  val leadFlightAttendant = context.actorOf(Props(LeadFlightAttendant()), config.getString(s"$configKey.leadAttendantName"))
+  val autopilot = context.actorOf(Props[Autopilot], "Autopilot")
 
   def receive = {
-    case AltitudeUpdate(altitudeInFeet) ⇒ log info (s"Altitude is now: $altitudeInFeet")
+    case AltitudeUpdate(altitudeInFeet) ⇒ log debug (s"Altitude is now: $altitudeInFeet")
     case GiveMeControl ⇒
       log info ("Plane giving control.")
-      sender ! controls
+      sender ! Controls(controls)
 
   }
 
   override def preStart() {
     altimeter ! RegisterListener(self)
+    pilot :: copilot :: Nil foreach { _ ! ReadyToGo }
   }
 
 }
---
+++ src/test/scala/zzz/akka/avionics/PlaneSpec.scala
@@ -0,0 +1,54 @@
+package zzz.akka.avionics
+
+import akka.actor.ActorSystem
+import akka.testkit.{ ImplicitSender, ScopedTestKit, TestActorRef, TestKit }
+import org.specs2.mutable.Specification
+
+class PlaneSpec extends Specification {
+  isolated
+  "Plane".title
+
+  "" should {
+    "relenquish controls when sent GiveMeControl" in new planeContext(ActorSystem("TestPlane")) {
+      val a = TestActorRef[Plane]
+
+      a ! Plane.GiveMeControl
+      expectMsgClass(classOf[Plane.Controls])
+    }
+
+    "accept AltitudeUpdates" in new planeContext(ActorSystem("TestPlane")) {
+      val a = TestActorRef[Plane]
+
+      a.receive(Altimeter.AltitudeUpdate(10.0))
+      expectNoMsg
+    }
+
+    "have a configKey of zzz.akka.avionics.flightcrew" in new planeContext(ActorSystem("TestPlane")) {
+      val a = TestActorRef[Plane]
+
+      a.underlyingActor.asInstanceOf[Plane].configKey must_== "zzz.akka.avionics.flightcrew"
+    }
+
+    "have a pilot with the name Harry" in new planeContext(ActorSystem("TestPlane")) {
+      val a = TestActorRef[Plane]
+      a.underlyingActor.asInstanceOf[Plane].pilot.path.name must contain("Harry")
+    }
+
+    "have a copilot with the name Joan" in new planeContext(ActorSystem("TestPlane")) {
+      val a = TestActorRef[Plane]
+      a.underlyingActor.asInstanceOf[Plane].copilot.path.name must contain("Joan")
+    }
+
+    "have an autopilot with the name Autopilot" in new planeContext(ActorSystem("TestPlane")) {
+      val a = TestActorRef[Plane]
+      a.underlyingActor.asInstanceOf[Plane].autopilot.path.name must contain("Autopilot")
+    }
+
+    "have a leadFlightAttendant with the name Gizalle" in new planeContext(ActorSystem("TestPlane")) {
+      val a = TestActorRef[Plane]
+      a.underlyingActor.asInstanceOf[Plane].leadFlightAttendant.path.name must contain("Gizalle")
+    }
+  }
+}
+
+class planeContext(val actorSystem: ActorSystem) extends TestKit(actorSystem) with ScopedTestKit with ImplicitSender
