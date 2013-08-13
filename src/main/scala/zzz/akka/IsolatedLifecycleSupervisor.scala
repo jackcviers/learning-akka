425442455c8d21df53fe5fae9f565c481c5d900e
Jack Viers <jackcviers@gmail.com>
2013-08-12 23:39:28 -0500

    Adds abstract Supervisors

--- src/main/scala/zzz/akka/IsolatedLifecycleSupervisor.scala
+++ src/main/scala/zzz/akka/IsolatedLifecycleSupervisor.scala
@@ -1,6 +1,32 @@
 package zzz.akka
 
-import akka.actor.Actor
+import akka.actor.{ Actor, ActorInitializationException, ActorKilledException }
+import akka.actor.SupervisorStrategy.{ Escalate, Resume, Stop }
+import scala.concurrent.duration.Duration
+
+abstract class IsolatedResumeSupervisor(
+  maxNumberOfRetries: Int = -1,
+  within: Duration = Duration.Inf)
+  extends IsolatedLifeCycleSupervisor { self: Supervisor ⇒
+  override val supervisorStrategy = strategy(maxNumberOfRetries, within) {
+    case _: ActorInitializationException ⇒ Stop
+    case _: ActorKilledException ⇒ Stop
+    case _: Exception ⇒ Resume
+    case _ ⇒ Escalate
+  }
+}
+
+abstract class IsolatedStopSupervisor(
+  maxNumberOfRetries: Int = -1,
+  within: Duration)
+  extends IsolatedLifeCycleSupervisor { self: Supervisor ⇒
+  override val supervisorStrategy = strategy(maxNumberOfRetries, within) {
+    case _: ActorInitializationException ⇒ Stop
+    case _: ActorKilledException ⇒ Stop
+    case _: Exception ⇒ Stop
+    case _ ⇒ Escalate
+  }
+}
 
 trait IsolatedLifeCycleSupervisor extends Actor {
   import IsolatedLifeCycleSupervisor.{ Started, WaitForStart }
