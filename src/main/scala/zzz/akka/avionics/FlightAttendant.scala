79a8646eda476ff795b47358ac50437521a5bf35
Jack Viers <jackcviers@gmail.com>
2013-08-31 21:51:27 -0500

    Adding Messages to handle stateful FlightAttendant

--- src/main/scala/zzz/akka/avionics/FlightAttendant.scala
+++ src/main/scala/zzz/akka/avionics/FlightAttendant.scala
@@ -1,6 +1,6 @@
 package zzz.akka.avionics
 
-import akka.actor.Actor
+import akka.actor.{ Actor, ActorRef }
 import scala.concurrent.duration._
 import scala.util.Random
 
@@ -21,6 +21,10 @@ trait AttendantResponsiveness {
 object FlightAttendant {
   case class GetDrink(drinkName: String)
   case class Drink(drinkName: String)
+  case class Assist(passenger: ActorRef)
+  case object Busy_?
+  case object Yes
+  case object No
   def apply() = new FlightAttendant with AttendantResponsiveness {
     val maxResponseTimeMS = 300000
   }
